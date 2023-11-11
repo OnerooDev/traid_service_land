d201ec861d5207a48303cd83b14f8acd97b954b8
SeerDay <70478004+SeerDay@users.noreply.github.com>
2023-11-12 04:59:16 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Logo } from '../components/images/Logo';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Traid Service</title>
+        <meta name="description" content="Traid Service - Надёжный поставщик метала" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <div className="hero">
+          <div className="container">
+            <Logo />
+            <p>Hi</p>
+          </div>
+        </div>
+    </>
+  )
+}
