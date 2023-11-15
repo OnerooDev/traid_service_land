0ca16d88558facd272707b41d9f5cc0e52d731ef
SeerDay <70478004+SeerDay@users.noreply.github.com>
2023-11-16 02:37:44 +0700

    go

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { Logo } from '../components/images/Logo';
 import { FoilRoll } from '../components/images/FoilRoll';
 import { BigBeam } from '../components/images/BigBeam';
+import { MetallBars } from '../components/images/MetallBars';
+import { ControlPanel } from '../components/images/ControlPanel';
+import { Frezer } from '../components/images/Frezer';
 
 export default function Home() {
   return (
@@ -18,11 +21,9 @@ export default function Home() {
             <div className="flex">
               <Logo />
               <div className='menu'>
-                <a href='#products'>Продукция</a>
-                <a href='#about'>О нас</a>
-                <div className='centered'>
-                  <a href='#contact'>Обратная cвязь</a>
-                </div>
+                <a className='menu_a' href='#products'>Продукция</a>
+                <a className='menu_a' href='#about'>О нас</a>
+                <a className='menu_a' href='#contact'>Обратная cвязь</a>
               </div>
             </div>
             <div className="flex padding_content">
@@ -31,9 +32,7 @@ export default function Home() {
                 <p className='description padding'>Поставщик высококачественного металлопроката</p>
                 <br />
                 <br />
-                <button>
-                  <p>Спектр услуг</p>
-                </button>
+                <a className='hero_a'>Спектр услуг</a>
               </div>
               <FoilRoll />
             </div>
@@ -57,6 +56,37 @@ export default function Home() {
                 <p className='head'>Транспортировка</p>
                 <p className='desc'>Мы имеем огромный парк большегрузных автомобилей</p>
                 <img src='/imgs/transport_photo.png' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='services'>
+          <div className="container">
+            <div className='head'>
+              <p className='header'>Спектр услуг</p>
+              <p className='description'>Качество - наш приоритет</p>
+            </div>
+            <div className='service_box'>
+              <div className='service_block'>
+                <MetallBars />
+                <p className='head'>МЕТАЛЛОПРОКАТ</p>
+                <p className='desc'>- Черный металлопрокат</p>
+                <p className='desc'>- Цветной металлопрокат</p>
+              </div>
+              <div className='service_block'>
+                <Frezer />
+                <p className='head'>МЕТАЛЛООБРАБОТКА</p>
+                <p className='desc'>- Инженерия - проектирование промышленных деталей и корпусов по чертежам и образцам заказчика</p>
+                <p className='desc'>- Токарная и фрезерная обработка на станках с ЧПУ и универсальных станках</p>
+                <p className='desc'>- Лазерная резка листового металла и труб</p>
+                <p className='desc'>- Гибка листового металла</p>
+                <p className='desc'>- Сварка лазером, полуавтоматом, аргоном, роботом</p>
+                <p className='desc'>- Гибка листового металла</p>
+              </div>
+              <div className='service_block'>
+                <ControlPanel />
+                <p className='head'>ЭКБ</p>
+                <p className='desc'>- Корпусная и узловая сборка электронных устройств</p>
               </div>
             </div>
           </div>
